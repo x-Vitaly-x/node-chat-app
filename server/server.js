6b5c1c6d8e4f197ccf2764550f4c29c4128c9c58
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2017-11-06 01:51:54 +0100

    socket emit logic

--- server/server.js
+++ server/server.js
@@ -15,6 +15,35 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New user!");
 
+    // socket.emit from Admin text - Welcome to chat app
+    // socket.broadcast.emit from Admin - New user joined
+
+    socket.emit("newMessage", {
+        from: "Admin",
+        text: "Welcome to the chat app!",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "New user joined!",
+        createdAt: new Date().getTime()
+    });
+
+    socket.on("createMessage", (message) => {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // }); // broadcast to entire socket
+
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }); // broadcast to everyone but you
+    });
+
     socket.on("disconnect", () => {
         console.log("User disconnected");
     });
