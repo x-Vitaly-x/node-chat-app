876542074911de2be5c1bee7ce65f89a6fbc7627
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2017-11-06 03:19:17 +0100

    form for commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const {generateMessage} = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
@@ -18,30 +19,13 @@ io.on("connection", (socket) => {
     // socket.emit from Admin text - Welcome to chat app
     // socket.broadcast.emit from Admin - New user joined
 
-    socket.emit("newMessage", {
-        from: "Admin",
-        text: "Welcome to the chat app!",
-        createdAt: new Date().getTime()
-    });
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app!"));
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "New user joined!",
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined!"));
 
-    socket.on("createMessage", (message) => {
-        // io.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // }); // broadcast to entire socket
-
-        socket.broadcast.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        }); // broadcast to everyone but you
+    socket.on("createMessage", (message, callback) => {
+        io.emit('newMessage', generateMessage(message.from, message.text)); // broadcast to everyone
+        callback('This is from the server');
     });
 
     socket.on("disconnect", () => {
