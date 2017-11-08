797a0e08c6db63112560b6f43a630ba889ce4b40
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2017-11-08 03:01:04 +0100

    geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const {generateMessage} = require("./utils/message");
+const {generateMessage, generateLocationMessage} = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
@@ -26,6 +26,10 @@ io.on("connection", (socket) => {
     socket.on("createMessage", (message, callback) => {
         io.emit('newMessage', generateMessage(message.from, message.text)); // broadcast to everyone
         callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', (coords, callback) => {
+        socket.broadcast.emit("newLocationMessage", generateLocationMessage("Admin", coords.latitude, coords.longitude));
     });
 
     socket.on("disconnect", () => {
