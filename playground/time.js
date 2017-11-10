01cd84f9ea232512e37c3e73163874b0aa2d5e06
Vitaly Stanchits <vitaly.stanchits@uni-potsdam.de>
2017-11-10 03:32:38 +0100

    mustache and time

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+var date = moment();
+
+//console.log(date.format('MMM Do, YYYY'));
+
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,25 @@ socket.on("disconnect", function () {
 });
 
 socket.on("newMessage", function (message) {
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 jQuery("#message-form").on('submit', function (e) {
